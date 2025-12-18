bbcebaee7225120ed0dc4e40a5d00322141ce14e
bala-amypo <bala@amypo.in>
2025-12-18 18:06:45 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskThresholdServiceImpl.java
+++ src/main/java/com/example/demo/service/RiskThresholdServiceImpl.java
@@ -1,24 +1,31 @@
 package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
+
 import com.example.demo.model.RiskThreshold;
 import com.example.demo.repository.RiskThresholdRepository;
 
 @Service
 public class RiskThresholdServiceImpl implements RiskThresholdService {
 
-    private final RiskThresholdRepository repo;
+    private final RiskThresholdRepository repository;
 
-    public RiskThresholdServiceImpl(RiskThresholdRepository repo) {
-        this.repo = repo;
+    public RiskThresholdServiceImpl(RiskThresholdRepository repository) {
+        this.repository = repository;
     }
 
-    public RiskThreshold save(RiskThreshold r) {
-        return repo.save(r);
+    public RiskThreshold createThreshold(RiskThreshold threshold) {
+
+        if (threshold.getMaxSingleStockPercentage() < 0 ||
+            threshold.getMaxSingleStockPercentage() > 100) {
+            throw new IllegalArgumentException("Invalid percentage");
+        }
+
+        return repository.save(threshold);
     }
 
-    public RiskThreshold getActive() {
-        return repo.findByActiveTrue()
-                .orElseThrow(() -> new RuntimeException("Not found"));
+    public RiskThreshold getThresholdById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Threshold not found"));
     }
 }
