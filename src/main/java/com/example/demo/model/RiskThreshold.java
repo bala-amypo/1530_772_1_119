58b703a487ece4e531d594cf7b4f0f60e2122e29
bala-amypo <bala@amypo.in>
2025-12-18 16:11:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RiskThreshold.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class RiskThreshold {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String thresholdName;
+    private Double maxSingleStockPercentage;
+    private Double maxSectorPercentage;
+    private Boolean active;
+
+    public RiskThreshold() {}
+
+    // getters and setters
+}
