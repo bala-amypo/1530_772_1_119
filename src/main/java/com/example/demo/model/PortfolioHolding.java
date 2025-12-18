3a0b9ccb460ab5bd717c661a601fef44564d5d76
bala-amypo <bala@amypo.in>
2025-12-18 09:07:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/PortfolioHolding.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class PortfolioHolding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserPortfolio portfolio;
+
+    @ManyToOne
+    private Stock stock;
+
+    private Double quantity;
+    private BigDecimal marketValue;
+
+    public PortfolioHolding() {}
+
+    // getters and setters
+}
