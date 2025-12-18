16ad36acb1cd71f63c792ff3fd18eee0cca3c9e7
bala-amypo <bala@amypo.in>
2025-12-18 17:58:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/StockRepository.java
+++ src/main/java/com/example/demo/repository/StockRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import com.example.demo.model.Stock;
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Optional<Stock> findByTicker(String ticker);
+
+    List<Stock> findBySector(String sector);
 }
