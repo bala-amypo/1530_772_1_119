1398541984c1f834ff20c1dd970dcba6161c1b9b
bala-amypo <bala@amypo.in>
2025-12-21 09:01:08 +0000

    User commit

--- src/main/java/com/example/demo/model/Stock.java
+++ src/main/java/com/example/demo/model/Stock.java
@@ -11,17 +11,20 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String ticker;
 
     private String companyName;
+
     private String sector;
-    private Boolean isActive = true;
+
+    private Boolean isActive;
 
     @OneToMany(mappedBy = "stock")
     private List<PortfolioHolding> holdings;
 
-    public Stock() {}
+    public Stock() {
+    }
 
     public Stock(String ticker, String companyName, String sector, Boolean isActive) {
         this.ticker = ticker;
@@ -30,13 +33,51 @@ public class Stock {
         this.isActive = isActive;
     }
 
-    public Long getId() { return id; }
-    public String getTicker() { return ticker; }
-    public void setTicker(String ticker) { this.ticker = ticker; }
-    public String getCompanyName() { return companyName; }
-    public void setCompanyName(String companyName) { this.companyName = companyName; }
-    public String getSector() { return sector; }
-    public void setSector(String sector) { this.sector = sector; }
-    public Boolean getIsActive() { return isActive; }
-    public void setIsActive(Boolean isActive) { this.isActive = isActive; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+    
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+    
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+    
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public List<PortfolioHolding> getHoldings() {
+        return holdings;
+    }
+
+    public void setHoldings(List<PortfolioHolding> holdings) {
+        this.holdings = holdings;
+    }
 }
