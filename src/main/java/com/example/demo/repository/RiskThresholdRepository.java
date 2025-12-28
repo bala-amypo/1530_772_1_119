e5c43cfa509d1b55e6df01beca45b02f93b7ccfa
bala-amypo <bala@amypo.in>
2025-12-28 06:41:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskThresholdRepository.java
+++ src/main/java/com/example/demo/repository/RiskThresholdRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RiskThresholdRepository extends JpaRepository<RiskThreshold, Long> {
+public interface RiskThresholdRepository
+        extends JpaRepository<RiskThreshold, Long> {
 
-    Optional<RiskThreshold> findByPortfolioId(Long portfolioId);
+    Optional<RiskThreshold> findFirstByActiveTrue();
 }
--- src/main/java/com/example/demo/repository/StockRepository.java
+++ src/main/java/com/example/demo/repository/StockRepository.java
@@ -3,12 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
-    Optional<Stock> findByTicker(String ticker);
-
-    List<Stock> findBySector(String sector);
+    Stock findByTicker(String ticker);
 }
